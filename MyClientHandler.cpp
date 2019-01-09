178e90ee8105d3c63a24111949ed1111b8e0dd76
okopel <okopel@gmail.com>
2019-01-09 04:00:13 -0800

    v 1.8

--- MyClientHandler.cpp
+++ MyClientHandler.cpp
@@ -17,42 +17,11 @@ void MyClientHandler<P, S>::handleClient(istream istream, ostream ostream) {
     int numOfGraphs = stoi(s);
     //load all graphs from inputStream
     for (int graphIndex = 0; graphIndex < numOfGraphs; graphIndex++) {
-        s = "";
-        istream >> s;//todo heg,width of h=w?
-        int size = stoi(s);
-        s = "";
-        istream >> s;
-        vector<int> pair = this->parserLine(s);
-        int initionX = pair[0];
-        int initionY = pair[1];
-        s = "";
-        istream >> s;
-        pair = this->parserLine(s);
-        int goalX = pair[0];
-        int goalY = pair[1];
-        //int height=stoi(s);        //int width;
-        State<int> ***matrix = new State<int> **[size];
-        //while (s != "end") {
-        for (int i = 0; i < size; i++) {//get the graph
-            vector<int> vector = this->parserLine(s);
-            matrix[i] = new State<int> *[size];
-            for (int j = 0; j < size; j++) {//get line of matrix
-                matrix[i][j] = new State<int>(vector[j]);
-            }
-            i++;
-            s = "";
-            istream >> s;
-        }
-        ostream << s;
-        Matrix<int> *myMatrix = new Matrix<int>(matrix, size, size, initionX, initionY, goalX, goalY);
-        matrixes.push_back(myMatrix);
-    }//enf of geting all the graphs.
-    //for (Matrix<int> *matrix:matrixes) {
+        matrixes.push_back(this->getMatrix(istream));
+    }
     for (Searchable<int> *tmpMatrix:matrixes) {
         this->solver->solve(tmpMatrix);
     }
-
-
 }
 
 template<class P, class S>
@@ -67,5 +36,34 @@ vector<int> MyClientHandler<P, S>::parserLine(string s) {
         }
         buffer += c;
     }
+    vector.push_back(stoi(buffer));
     return vector;
+}
+
+template<class P, class S>
+Matrix<int> *MyClientHandler<P, S>::getMatrix(istream istream) {
+    string s = "";
+    istream >> s;//todo heg,width of h=w?
+    int size = stoi(s);
+    s = "";
+    istream >> s;
+    vector<int> pair = this->parserLine(s);
+    int initionX = pair[0];
+    int initionY = pair[1];
+    s = "";
+    istream >> s;
+    pair = this->parserLine(s);
+    int goalX = pair[0];
+    int goalY = pair[1];
+    State<int> ***matrix = new State<int> **[size];
+    for (int i = 0; i < size; i++) {//get the graph
+        vector<int> vector = this->parserLine(s);
+        matrix[i] = new State<int> *[size];//initon line
+        for (int j = 0; j < size; j++) {//get line of matrix
+            matrix[i][j] = new State<int>(vector[j]);
+        }
+        s = "";
+        istream >> s;
+    }
+    return new Matrix<int>(matrix, size, size, initionX, initionY, goalX, goalY);
 }
--- MyClientHandler.h
+++ MyClientHandler.h
@@ -3,12 +3,15 @@
 
 #include "./interface/ClientHandler.h"
 #include <vector>
+#include "searchable/Matrix.h"
 
 using std::string;
 
 template<class P, class S>
 class MyClientHandler : public ClientHandler<P, S> {
     vector<int> parserLine(string s);
+
+    Matrix<int> *getMatrix(istream istream);
 
 public:
     void handleClient(istream istream, ostream ostream) override;
